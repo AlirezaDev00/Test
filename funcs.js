334aa00119c6c78694ab92094d7d96d84e0fd970
ALIREZA (F.C.B) <alirezadevloperui@gmail.com>
2025-03-26 16:05:23 +0330

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -1,15 +1,3 @@
 const sign_in = () => {
     console.log("sign_in");
 }
-
-const login = () => {
-    console.log("login");
-}
-
-const password = () => {
-    console.log("password");
-}
-
-const email_validation = () => {
-    return true 
-}
