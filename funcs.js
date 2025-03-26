e3416274eaab846b2c240be4ab77dcfda629f633
AlirezDev00 <alirezadevloperui@gmail.com>
2025-03-26 17:09:08 +0330

    dashagh

--- funcs.js
+++ funcs.js
@@ -1,3 +1,14 @@
 const sign_in = () => {
-    return true
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    let data = { email: email, password: password }
+    // fetch('/sign_in', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data)
+    // })
+    // .then(response => response.json())
+    return false
 }
